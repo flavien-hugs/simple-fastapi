0afdbba99d6eee07a707801f6c0f7728f0244355
flavien-hugs <geekactux@gmail.com>
2022-12-18 12:28:18 +0000

    Update to flake8 config

--- core/main.py
+++ core/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .config.config import settings
 
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     return app
