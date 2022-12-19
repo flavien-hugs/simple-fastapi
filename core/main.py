0c798688da647ee12d9efbb19a2e436ae048a201
flavien-hugs <geekactux@gmail.com>
2022-12-19 18:43:44 +0000

    Add routers

--- core/main.py
+++ core/main.py
@@ -1,10 +1,36 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .db.base import Base
+from .db.session import engine
+from .api.base import api_router
 from .config.config import settings
+
+
+def create_tables():
+    print("Create database tables")
+    Base.metadata.create_all(bind=engine)
+
+
+def include_routers(app):
+    app.include_router(api_router)
 
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    create_tables()
+    include_routers(app)
     return app
 
 
 app = start_application()
+
+"""
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8070"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+"""
