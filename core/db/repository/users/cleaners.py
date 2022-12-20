2da17d0e2ec2d9a06c20a5d7692b5bd13beb5c0c
flavien-hugs <geekactux@gmail.com>
2022-12-20 16:49:45 +0000

    Update user CRUD function

--- core/db/repository/users/cleaners.py
+++ core/db/repository/users/cleaners.py
@@ -1,23 +1,15 @@
-from typing import List
-
 from sqlalchemy.orm import Session
 
+from core.db.models.constants import UserType
 from core.db.models.users import User
-from core.db.models.constants import Gender, UserType
-from core.schemas.users.cleaners import UserCleanerCreate, UserCleanerUpdate
+from core.schemas.users.cleaners import UserCleanerCreate
+from core.schemas.users.cleaners import UserCleanerUpdate
 
 
 def create_cleaner_user(user: UserCleanerCreate, db: Session):
-    cleaner = User(
-        lbm_gender=List[Gender],
-        lbm_user_type=UserType.CLEANING,
-        lbm_username=user.lbm_username,
-        lbm_addr_email=user.lbm_addr_email,
-        lbm_district=user.lbm_district,
-        lbm_phone_number_one=user.lbm_phone_number_one,
-        lbm_phone_number_two=user.lbm_phone_number_two,
-        lbm_is_active=True
-    )
+    cleaner = User(**user.dict())
+    cleaner.lbm_user_type = UserType.CLEANER
+    cleaner.lbm_is_active = True
     db.add(cleaner)
     db.commit()
     db.refresh(cleaner)
@@ -25,13 +17,25 @@ def create_cleaner_user(user: UserCleanerCreate, db: Session):
     return cleaner
 
 
-def get_cleaners(db: Session):
-    users = db.query(User).filter(User.lbm_is_active, User.lbm_user_type == UserType.CLEANING).all()
+def get_cleaners(db: Session, skip: int = 0, limit: int = 100):
+    users = (
+        db.query(User)
+        .filter(User.lbm_is_active, User.lbm_user_type == UserType.CLEANER)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
     return users
 
 
-def get_user_cleaner(cleaner_id: int, db: Session):
-    return db.query(User).filter(User.id == cleaner_id, User.lbm_user_type == UserType.CLEANING).first()
+def get_cleaner(id: int, db: Session):
+    return (
+        db.query(User)
+        .filter(
+            User.id == id, User.lbm_is_active, User.lbm_user_type == UserType.CLEANER
+        )
+        .first()
+    )
 
 
 def get_cleaner_by_district(district: str, db: Session):
@@ -39,8 +43,8 @@ def get_cleaner_by_district(district: str, db: Session):
     return user
 
 
-def update_cleaner_by_id(cleaner_id: int, user: UserCleanerUpdate, db: Session):
-    existing_user = db.query(User).filter(User.id == cleaner_id)
+def update_cleaner_by_id(id: int, user: UserCleanerUpdate, db: Session):
+    existing_user = db.query(User).filter(User.id == id)
     if not existing_user.first():
         return 0
     user.__dict__.update()
--- core/db/repository/users/customers.py
+++ core/db/repository/users/customers.py
@@ -1,21 +1,17 @@
 from sqlalchemy.orm import Session
 
-from core.db.models.users import User
 from core.config.hashing import Hasher
 from core.db.models.constants import UserType
-from core.schemas.users.customers import UserCustomerCreate, UserCustomerUpdate
+from core.db.models.users import User
+from core.schemas.users.customers import UserCustomerCreate
+from core.schemas.users.customers import UserCustomerUpdate
 
 
-def create_customer_user(user: UserCustomerCreate, db: Session):
-    user = User(
-        lbm_user_type=UserType.CUSTOMER,
-        lbm_username=user.lbm_username,
-        lbm_addr_email=user.lbm_addr_email,
-        lbm_phone_number_one=user.lbm_phone_number_one,
-        lbm_district=user.lbm_district,
-        lbm_password=Hasher.get_password_hash(user.lbm_password),
-        lbm_is_active=True
-    )
+def create_customer(user: UserCustomerCreate, db: Session):
+    user = User(**user.dict())
+    user.lbm_user_type = UserType.CUSTOMER
+    user.lbm_password = Hasher.get_password_hash(user.lbm_password)
+    user.lbm_is_active = True
     db.add(user)
     db.commit()
     db.refresh(user)
@@ -23,34 +19,45 @@ def create_customer_user(user: UserCustomerCreate, db: Session):
     return user
 
 
-def get_users_customers(db: Session):
-    users = db.query(User).filter(User.lbm_is_active, User.lbm_user_type == UserType.CUSTOMER).all()
+def get_customers(db: Session, skip: int = 0, limit: int = 100):
+    users = (
+        db.query(User)
+        .filter(User.lbm_is_active, User.lbm_user_type == UserType.CUSTOMER)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
     return users
 
 
-def get_user_customer(customer_id: int, db: Session):
-    return db.query(User).filter(User.id == customer_id, User.lbm_user_type == UserType.CUSTOMER).first()
+def get_customer(id: int, db: Session):
+    return (
+        db.query(User)
+        .filter(
+            User.id == id, User.lbm_is_active, User.lbm_user_type == UserType.CUSTOMER
+        )
+        .first()
+    )
 
 
-def get_user_by_district(district: str, db: Session):
-    user = db.query(User).filter(User.lbm_district == district).first()
-    return user
-
-
-def update_user_by_id(customer_id: int, user: UserCustomerUpdate, db: Session):
-    existing_user = db.query(User).filter(User.id == customer_id)
-    if not existing_user.first():
+def update_customer(id: int, user: UserCustomerUpdate, db: Session):
+    existing_customer = db.query(User).filter(
+        User.id == id, User.lbm_user_type == UserType.CUSTOMER
+    )
+    if not existing_customer.first():
         return 0
     user.__dict__.update()
-    existing_user.update(user.__dict__)
+    existing_customer.update(user.__dict__)
     db.commit()
     return 1
 
 
-def delete_user_by_id(customer_id: int, db: Session):
-    existing_user = db.query(User).filter(User.id == customer_id)
-    if not existing_user.first():
+def delete_customer(id: int, db: Session):
+    existing_customer = db.query(User).filter(
+        User.id == id, User.lbm_user_type == UserType.CUSTOMER
+    )
+    if not existing_customer.first():
         return 0
-    existing_user.delete(synchronize_session=False)
+    existing_customer.delete(synchronize_session=False)
     db.commit()
     return 1
