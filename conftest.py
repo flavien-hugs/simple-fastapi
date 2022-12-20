0cf89bd13f863cfa0acbe602ce984403007a63ad
flavien-hugs <geekactux@gmail.com>
2022-12-20 16:43:43 +0000

    Update route api entrypoint

---
+++ conftest.py
@@ -0,0 +1,70 @@
+import os
+import sys
+from typing import Any
+from typing import Generator
+
+import pytest
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+
+from core.db.base import Base
+from core.db.session import get_db
+from core.api.base import api_router
+from core.config.config import settings
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+def start_application():
+    app = FastAPI()
+    app.include_router(api_router)
+    return app
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="module")
+def app() -> Generator[FastAPI, Any, None]:
+    """
+    Create a fresh database on each test case.
+    """
+    Base.metadata.create_all(engine)
+    _app = start_application()
+    yield _app
+    Base.metadata.drop_all(engine)
+
+
+@pytest.fixture(scope="module")
+def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = SessionTesting(bind=connection)
+    yield session
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
+@pytest.fixture(scope="module")
+def client(app: FastAPI, db_session: SessionTesting) -> Generator[TestClient, Any, None]:
+    """
+    Create a new FastAPI TestClient that uses the `db_session` fixture to override
+    the `get_db` dependency that is injected into routes.
+    """
+
+    def _get_test_db():
+        try:
+            yield db_session
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = _get_test_db
+    with TestClient(app) as client:
+        yield client
---
+++ core/schemas/base.py
@@ -0,0 +1,2 @@
+from .users.cleaners import UserCleanerCreate, UserCleanerOut, UserCleanerUpdate
+from .users.customers import UserCustomerCreate, UserCustomerOut, UserCustomerUpdate
