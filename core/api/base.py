91d4b1fd372a62715379acd7f9a3f10a4f9c40fa
flavien-hugs <geekactux@gmail.com>
2022-12-20 16:48:59 +0000

    Update api routes entrypoint

--- core/api/base.py
+++ core/api/base.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
 
-from core.api.v1.users.cleaner import user_routes as cleaner_route
-from core.api.v1.users.customer import user_routes as customer_router
+from .v1.users.cleaner import user_routes as cleaner_route
+from .v1.users.customer import user_routes as customer_router
 
 api_router = APIRouter()
 
 
-api_router.include_router(cleaner_route.cleaners_router, prefix="/cleaner", tags=["cleaner"])
-api_router.include_router(customer_router.customers_router, prefix="/customer", tags=["customer"])
+api_router.include_router(
+    cleaner_route.cleaners_router, prefix="/cleaner", tags=["cleaner"]
+)
+api_router.include_router(
+    customer_router.customers_router, prefix="/customer", tags=["customer"]
+)
