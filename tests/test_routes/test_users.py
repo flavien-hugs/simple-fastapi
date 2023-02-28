e860cac74cebae898b74e6270c47efbdf5505f33
BEF <geekactux@gmail.com>
2023-02-28 10:12:44 +0000

    update test configuration

--- tests/test_routes/test_users.py
+++ tests/test_routes/test_users.py
@@ -1,10 +1,8 @@
-import json
-
 def test_get_customers(client):
-    response = client.get("/customer/")
+    response = client.get("/api/v1/user/")
     assert response.status_code == 200
 
 
 def test_get_cleaner(client):
-    response = client.get("/cleaner/")
+    response = client.get("/api/v1/cleaner/")
     assert response.status_code == 200
